fd9fea2809d9e46c8cc5cf0963a83c743b3899be
Praveen <47313992+praveenshrivas@users.noreply.github.com>
2026-08-18 19:40:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "demo" {
   ami           = "ami-0912f71e06545ad88"
   instance_type = "t3.micro"
 
+  key_name = "New-Data-Class-Account"
+
   tags = {
     Name = "github-actions-demo"
   }
